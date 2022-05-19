60a864f587b6cdfe2a6a2761457c8de8446da0ba
ParkSolHwi <psh50zmfhtm@gmail.com>
2022-05-19 21:38:12 +0900

    [솔휘] 로그인 세션 종료자로 인해 GC 성능 저하 개선
    ㅈㄱㄴ

--- FightingGame/Assets/Scripts/Network/Login/LoginSession.cs
+++ FightingGame/Assets/Scripts/Network/Login/LoginSession.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 /// <summary>
 /// 로그인 정보가 남아있는 세션, 한 번 인증한 후엔 더 이상 인증을 할 수 없도록 세션만 남기는 것을 추천
 /// </summary>
-public class LoginSession
+public class LoginSession : IDisposable
 {
+    private bool disposedValue;
+
     public static bool IsSessionValid
     {
         get
@@ -34,8 +37,33 @@ public class LoginSession
         authInfo = null;
     }
 
-    ~LoginSession()
+    protected virtual void Dispose(bool disposing)
     {
-        UnregisterAuth();
+        if (!disposedValue)
+        {
+            if (disposing)
+            {
+                // TODO: 관리형 상태(관리형 개체)를 삭제합니다.
+                UnregisterAuth();
+            }
+
+            // TODO: 비관리형 리소스(비관리형 개체)를 해제하고 종료자를 재정의합니다.
+            // TODO: 큰 필드를 null로 설정합니다.
+            disposedValue = true;
+        }
+    }
+
+    // // TODO: 비관리형 리소스를 해제하는 코드가 'Dispose(bool disposing)'에 포함된 경우에만 종료자를 재정의합니다.
+    // ~LoginSession()
+    // {
+    //     // 이 코드를 변경하지 마세요. 'Dispose(bool disposing)' 메서드에 정리 코드를 입력합니다.
+    //     Dispose(disposing: false);
+    // }
+
+    public void Dispose()
+    {
+        // 이 코드를 변경하지 마세요. 'Dispose(bool disposing)' 메서드에 정리 코드를 입력합니다.
+        Dispose(disposing: true);
+        GC.SuppressFinalize(this);
     }
 }
