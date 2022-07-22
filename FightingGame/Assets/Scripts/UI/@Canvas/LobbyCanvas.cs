fbae3985e81bfdf84b49412cc62b7b041ca704e7
YataNox <rladnwls0523@naver.com>
2022-07-23 01:46:06 +0900

    [우진] 미구현 버튼들 Window 활성화 기능만 선 구현
    CustomWindow, TrainingBtn,Window 활성화

--- FightingGame/Assets/Scripts/UI/@Canvas/LobbyCanvas.cs
+++ FightingGame/Assets/Scripts/UI/@Canvas/LobbyCanvas.cs
@@ -6,16 +6,20 @@ using FGDefine;
 public class LobbyCanvas : BaseCanvas
 {
     [SerializeField] MatchWindow matchWindow;
+    [SerializeField] CustomWindow customWindow;
     [SerializeField] RankWindow rankWindow;
     [SerializeField] CharacterWindow characterWindow;
+    [SerializeField] TrainingWindow trainingWindow;
     [SerializeField] SettingWindow settingWindow;
     [SerializeField] MatchTimeWindow matchTimeWindow;
 
     public override void Open<T>(UIParam param = null) // UIMgr.Open<> 세분화
     {
         if (typeof(T) == typeof(MatchWindow)) matchWindow.Open();
+        else if (typeof(T) == typeof(CustomWindow)) customWindow.Open();
         else if (typeof(T) == typeof(RankWindow)) rankWindow.Open();
         else if (typeof(T) == typeof(CharacterWindow)) characterWindow.Open();
+        else if (typeof(T) == typeof(TrainingWindow)) trainingWindow.Open();
         else if (typeof(T) == typeof(SettingWindow)) settingWindow.Open();
         else if (typeof(T) == typeof(MatchTimeWindow)) matchTimeWindow.Open();
         else Debug.Log("범위 벗어남");
@@ -24,8 +28,10 @@ public class LobbyCanvas : BaseCanvas
     public override void Close<T>() // UIMgr.Close<> 세분화
     {
         if (typeof(T) == typeof(MatchWindow)) matchWindow.Close();
+        else if (typeof(T) == typeof(CustomWindow)) customWindow.Close();
         else if (typeof(T) == typeof(RankWindow)) rankWindow.Close();
         else if (typeof(T) == typeof(CharacterWindow)) characterWindow.Close();
+        else if (typeof(T) == typeof(TrainingWindow)) trainingWindow.Close();
         else if (typeof(T) == typeof(SettingWindow)) settingWindow.Close();
         else if (typeof(T) == typeof(MatchTimeWindow)) matchTimeWindow.Close();
         else Debug.Log("범위 벗어남");
@@ -38,11 +44,17 @@ public class LobbyCanvas : BaseCanvas
             case "Match":
                 Managers.UI.OpenUI<MatchWindow>();
                 break;
+            case "Custom":
+                Managers.UI.OpenUI<CustomWindow>();
+                break;
             case "Rank":
                 Managers.UI.OpenUI<RankWindow>();
                 break;
             case "Character":
                 Managers.UI.OpenUI<CharacterWindow>();
+                break;
+            case "Training":
+                Managers.UI.OpenUI<TrainingWindow>();
                 break;
             case "Setting":
                 Managers.UI.OpenUI<SettingWindow>();
@@ -57,11 +69,17 @@ public class LobbyCanvas : BaseCanvas
             case "Match":
                 Managers.UI.CloseUI<MatchWindow>();
                 break;
+            case "Custom":
+                Managers.UI.CloseUI<CustomWindow>();
+                break;
             case "Rank":
                 Managers.UI.CloseUI<RankWindow>();
                 break;
             case "Character":
                 Managers.UI.CloseUI<CharacterWindow>();
+                break;
+            case "Training":
+                Managers.UI.CloseUI<TrainingWindow>();
                 break;
             case "Setting":
                 Managers.UI.CloseUI<SettingWindow>();
