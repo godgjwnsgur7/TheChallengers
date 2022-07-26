14dc4ce3491b622541a2b8da5b4db3e79a2fe6a2
YataNox <rladnwls0523@naver.com>
2022-07-26 14:56:00 +0900

    [우진] 커스텀 매치 창 구현 중...
    Canvas Script 폴더 이름 변경 : MainCanvas -> LobbyCanvas
    StartCanvas -> MainCanvas
    
    CustomWindow에 방 생성 panel 생성

---
+++ FightingGame/Assets/Scripts/UI/LobbyCanvas/WindowUI/CustomWindow.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomWindow : UIElement
+{
+    [SerializeField] CustomPanel customPanel;
+    [SerializeField] CreateRoomPanel createRoomPanel;
+
+    private void OnEnable() // CustomWindow 활성화 시에 CustomPanel 활성화
+    {
+        if (createRoomPanel.gameObject.activeSelf)
+            CloseCreateRoomPanel();
+        else 
+            customPanel.Open();
+    }
+
+    public override void Open(UIParam param = null)
+    {
+        base.Open(param);
+    }
+
+    public override void Close()
+    {
+        base.Close();
+    }
+
+    public void OpenCreateRoomPanel() // 방 생성 패널 활성화
+    {
+        customPanel.Close();
+        createRoomPanel.Open();
+    }
+
+    public void CloseCreateRoomPanel() // 방 목록 패널 활성화
+    {
+        customPanel.Open();
+        createRoomPanel.Close();
+    }
+}
