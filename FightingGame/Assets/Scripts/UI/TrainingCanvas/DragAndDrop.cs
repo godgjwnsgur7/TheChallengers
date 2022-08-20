faeaf9be8eca44417142f2025273f71de5e74ff7
YataNox <rladnwls0523@naver.com>
2022-08-20 19:32:17 +0900

    [우진] 훈련장 UI 드래그 기능 추가중
    충돌 시 움직임은 미구현, 현재 드래그 하려는 UI는 부모 UI객체의 범위 안에서 자유롭게 드래그 가능

--- FightingGame/Assets/Scripts/UI/TrainingCanvas/DragAndDrop.cs
+++ FightingGame/Assets/Scripts/UI/TrainingCanvas/DragAndDrop.cs
@@ -4,13 +4,23 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class DragAndDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler, IDropHandler
+public class DragAndDrop : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDragHandler
 {
-
+    [SerializeField] GameObject parent;
+    RectTransform parentRectTransform;
     RectTransform rectTransform;
     CanvasGroup canvasGroup;
     Image image1;
-    [SerializeField] Canvas canvas;
+
+    Vector2 currentPosition;
+    Vector2 beforeVec;
+
+    float parentHalfWidth;
+    float parentHalfHeight;
+    float halfWidth;
+    float halfHeight;
+    float xRange;
+    float yRange;
 
     public bool isUpdate = false;
     public float AlphaThreshold = 0.1f;
@@ -19,6 +29,14 @@ public class DragAndDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler
     {
         rectTransform = GetComponent<RectTransform>();
         canvasGroup = GetComponent<CanvasGroup>();
+        parentRectTransform = parent.GetComponent<RectTransform>();
+
+        parentHalfWidth = parentRectTransform.sizeDelta.x / 2;
+        parentHalfHeight = parentRectTransform.sizeDelta.y / 2;
+        halfWidth = rectTransform.sizeDelta.x / 2;
+        halfHeight = rectTransform.sizeDelta.y / 2;
+
+        beforeVec = new Vector2(rectTransform.anchoredPosition.x, rectTransform.anchoredPosition.y);
 
         // Buttom Click 가능 범위 설정
         image1 = GetComponent<Image>();
@@ -41,7 +59,7 @@ public class DragAndDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler
 
         // 이전 이동과 비교해서 얼마나 이동했는지를 보여줌
         // 캔버스의 스케일과 맞춰야 하기 때문에
-        rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+        rectTransform.anchoredPosition += eventData.delta;
     }
 
     public void OnEndDrag(PointerEventData eventData)
@@ -49,17 +67,15 @@ public class DragAndDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler
         if (!isUpdate)
             return;
 
+        xRange = Mathf.Clamp(rectTransform.anchoredPosition.x, -parentHalfWidth + halfWidth, parentHalfWidth - halfWidth);
+        yRange = Mathf.Clamp(rectTransform.anchoredPosition.y, -parentHalfHeight + halfHeight, parentHalfHeight - halfHeight);
+
+        currentPosition = new Vector2(xRange, yRange);
+        rectTransform.anchoredPosition = currentPosition;
+
+        beforeVec = currentPosition;
+
         canvasGroup.alpha = 1f;
         canvasGroup.blocksRaycasts = true;
-    }
-
-    public void OnPointerDown(PointerEventData eventData)
-    {
-
-    }
-
-    public void OnDrop(PointerEventData eventData)
-    {
-
     }
 }
