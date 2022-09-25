fec6c62189f6d7dc2a7706f33d1a87cdda682718
godgjwnsgur7 <godgjwnsgur7@naver.com>
2022-09-25 20:17:10 +0900

    [준혁] 방 입장을 시도해보려고 하는 중
    .

--- FightingGame/Assets/Scripts/UI/LobbyCanvas/CustomRoomWindowUI.cs
+++ FightingGame/Assets/Scripts/UI/LobbyCanvas/CustomRoomWindowUI.cs
@@ -2,15 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Photon.Pun;
 
 public class CustomRoomWindowUI : MonoBehaviour
 {
+    [SerializeField] Text roomNameText;
+
     [SerializeField] Image currMapIamge;
     [SerializeField] Image nextMapIamge_Left;
     [SerializeField] Image nextMapIamge_Right;
 
     [SerializeField] CharProfileUI masterProfile;
     [SerializeField] CharProfileUI slaveProfile;
+
+    CharProfileUI myProfile;
+
+    public bool isInit = false;
+
+    public void Init()
+    {
+        if (isInit) return;
+
+        isInit = true;
+        // 현재 방 정보를 받아와야 함
+
+        if (PhotonLogicHandler.IsMasterClient)
+            myProfile = masterProfile;
+        else
+            myProfile = slaveProfile;
+    }
 
     public void OnClick_ChangeMap()
     {
@@ -19,13 +39,29 @@ public class CustomRoomWindowUI : MonoBehaviour
 
     public void OnClick_ExitRoom()
     {
-        // 누르자 마자 준비해제 되야함
+        // 누르자 마자 준비해제?
 
         Managers.UI.popupCanvas.Open_SelectPopup(ExitRoom, null, "정말 방에서 나가시겠습니까?");
     }
 
+    public void OnClick_Ready()
+    {
+        myProfile.OnClick_Ready();
+    }
+
+    public void OnClick_Start()
+    {
+        // 배틀 씬으로 같이 이동.?
+    }
+
     public void ExitRoom()
     {
-        Debug.Log("방을 나갑니다 ㅎㅎ - 미구현");
+        Clear();
+        PhotonLogicHandler.Instance.TryLeaveRoom();
+    }
+
+    private void Clear()
+    {
+
     }
 }
