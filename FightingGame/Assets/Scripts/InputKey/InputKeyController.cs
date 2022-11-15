f823347e0b03e128fce96411865dd6ce8e583d39
YataNox <rladnwls0523@naver.com>
2022-11-15 15:45:29 +0900

    [우진] ㅇㅇ

--- FightingGame/Assets/Scripts/InputKey/InputKeyController.cs
+++ FightingGame/Assets/Scripts/InputKey/InputKeyController.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using FGDefine;
+using System;
 
 public class InputKeyController : MonoBehaviour
 {
     List<KeySettingData> keySettingDataList = null;
+
+    private InputKeyManagement inputKeyManagement = null;
+    private PlayerCharacter currPlayer;
 
     public InputPanel inputPanel = null;
     private RectTransform panelTr = null;
@@ -20,24 +24,93 @@ public class InputKeyController : MonoBehaviour
         if (inputPanel == null)
         {
             inputPanel = Managers.Resource.Instantiate("UI/InputPanel", this.transform).GetComponent<InputPanel>();
-            inputPanel.Init(null, null);
+            inputPanel.Init(PointerDown, PointerUp);
             panelTr = inputPanel.GetComponent<RectTransform>();
         }
 
         Set_keySettingDataList();
         Set_PanelActive(true);
 
-        InputKeyManagement Ikm = gameObject.transform.parent.Find("InputKeyManagement").GetComponent<InputKeyManagement>();
-        if (Ikm == null)
+        /*inputKeyManagement = gameObject.transform.parent.Find("InputKeyManagement").GetComponent<InputKeyManagement>();
+        if (inputKeyManagement == null)
             return;
 
-        if (Ikm.isPanelActive)
-            Ikm.Set_PanelActive(false);
+        if (inputKeyManagement.isPanelActive)
+            inputKeyManagement.Set_PanelActive(false);*/
     }
+
+    public void Connect_Player(PlayerCharacter _player) => currPlayer = _player;
+    public void Disconnect_Player() => currPlayer = null;
 
     public void PointerDown(InputKey _inputkey)
     {
+        if (currPlayer == null)
+            return;
 
+        // Attack
+        if (_inputkey.name.Equals(Enum.GetName(typeof(ENUM_INPUTKEY_NAME), 2)))
+        {
+            int AttackObject1_Num = (int)currPlayer.activeCharacter.characterType * 10 + 1;
+            CharacterAttackParam attackParam = new CharacterAttackParam((ENUM_ATTACKOBJECT_NAME)AttackObject1_Num, currPlayer.activeCharacter.reverseState);
+            currPlayer.PlayerCommand(ENUM_PLAYER_STATE.Attack, attackParam);
+            currPlayer.activeCharacter.Change_AttackState(true);
+        }
+
+        // Skill1
+        if (_inputkey.name.Equals(Enum.GetName(typeof(ENUM_INPUTKEY_NAME), 3)))
+        {
+            CharacterSkillParam skillParam1 = new CharacterSkillParam(0);
+            currPlayer.PlayerCommand(ENUM_PLAYER_STATE.Skill, skillParam1);
+        }
+
+        // Skill2
+        if (_inputkey.name.Equals(Enum.GetName(typeof(ENUM_INPUTKEY_NAME), 4)))
+        {
+            CharacterSkillParam skillParam2 = new CharacterSkillParam(1);
+            currPlayer.PlayerCommand(ENUM_PLAYER_STATE.Skill, skillParam2);
+        }
+
+        // Skill3
+        if (_inputkey.name.Equals(Enum.GetName(typeof(ENUM_INPUTKEY_NAME), 5)))
+        {
+            CharacterSkillParam skillParam3 = new CharacterSkillParam(2);
+            currPlayer.PlayerCommand(ENUM_PLAYER_STATE.Skill, skillParam3);
+        }
+
+        // Jump
+        if (_inputkey.name.Equals(Enum.GetName(typeof(ENUM_INPUTKEY_NAME), 6)))
+        {
+            currPlayer.PlayerCommand(ENUM_PLAYER_STATE.Jump);
+        }
+
+        // LeftArrow
+        if (_inputkey.name.Equals(Enum.GetName(typeof(ENUM_INPUTKEY_NAME), 0)))
+            currPlayer.moveDir = -1.0f;
+
+        // RightArrow
+        if (_inputkey.name.Equals(Enum.GetName(typeof(ENUM_INPUTKEY_NAME), 1)))
+            currPlayer.moveDir = 1.0f;
+
+        if (currPlayer.moveDir != 0f)
+        {
+            currPlayer.activeCharacter.Input_MoveKey(true);
+            currPlayer.PlayerCommand(ENUM_PLAYER_STATE.Move, new CharacterMoveParam(currPlayer.moveDir));
+        }
+    }
+
+    public void PointerUp(InputKey _inputkey)
+    {
+        if(currPlayer.activeCharacter.anim.GetBool("AttackState"))
+            currPlayer.activeCharacter.Change_AttackState(false);
+
+        if (currPlayer.moveDir != 0f)
+        {
+            currPlayer.moveDir = 0f;
+            currPlayer.activeCharacter.Input_MoveKey(false);
+
+            if (currPlayer.activeCharacter.currState == ENUM_PLAYER_STATE.Move)
+                currPlayer.PlayerCommand(ENUM_PLAYER_STATE.Idle);
+        }
     }
 
     // PlayerPrefs 값 호출
