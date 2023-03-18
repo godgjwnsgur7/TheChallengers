1214b80a12e711c52cc3ec5f6a827d77ff16c163
ParkSolHwi <psh50zmfhtm@gmail.com>
2023-03-18 21:37:15 +0900

    [솔휘] 경로가 이상하여 프리팹 생성되지 않는 경우 null 처리 추가

---
+++ FightingGame/Assets/Scripts/Config/PlayerConfig.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+#if UNITY_EDITOR
+
+[InitializeOnLoad]
+public class PlayerConfig
+{
+	const string password = "123456";
+	const string keyPath = "UserSettings/user.keystore";
+	const string projectKey = "fightinggame";
+
+	static PlayerConfig()
+	{
+#if UNITY_ANDROID
+
+		PlayerSettings.Android.keystoreName = keyPath;
+		PlayerSettings.keystorePass = password;
+
+		PlayerSettings.Android.keyaliasName = projectKey;
+		PlayerSettings.keyaliasPass = password;
+#endif
+	}
+
+#endif
+}
--- FightingGame/Assets/Scripts/Network/Photon/PhotonPrefabPool.cs
+++ FightingGame/Assets/Scripts/Network/Photon/PhotonPrefabPool.cs
@@ -13,9 +13,16 @@ public class PhotonPrefabPool : IPunPrefabPool
 	public GameObject Instantiate(string prefabId, Vector3 position, Quaternion rotation)
 	{
 		var obj = Managers.Resource.Instantiate(prefabId, position, rotation, false);
-		
+		if (obj != null)
+		{
+			Debug.LogError($"{prefabId} 경로가 잘못되어 프리팹을 불러올 수 없습니다.");
+			return null;
+		}
+
 		if (obj.activeSelf) // 혹시 활성화되버리는 경우
+		{
 			obj.SetActive(false);
+		}
 
 		return obj;
 	}
