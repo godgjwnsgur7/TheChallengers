d66f1c1d3b5d791fe1e58e4abe5538b2bee6e899
psh50zmfhtm <psh50zmfhtm@naver.com>
2023-05-01 16:08:26 +0900

    마스터 클라이언트만 부를 수도 있게 변경

--- FightingGame/Assets/Scripts/Network/Photon/PhotonLogicHandler.Helper.cs
+++ FightingGame/Assets/Scripts/Network/Photon/PhotonLogicHandler.Helper.cs
@@ -230,7 +230,7 @@ public partial class PhotonLogicHandler
 
     // 기본적으로 PhotonLogicHandler를 제외한 현재 모든 씬의 포톤 객체를 삭제시킨다.
     // 제외하기 원하는 타입을 매개변수로 넘길 수 있음
-    public IEnumerator TryDestroyAllPhotonOnScene(params Type[] ignoreTypes)
+    public IEnumerator TryDestroyAllPhotonOnScene(bool doJustMasterClient, params Type[] ignoreTypes)
     {
         var photons = FindObjectsOfType<MonoBehaviourPhoton>();
         if (photons == null)
@@ -241,7 +241,11 @@ public partial class PhotonLogicHandler
         foreach(var photon in photons)
         {
             yield return null;
-            TryDestroy(photon);
+
+            if(IsMasterClient || !doJustMasterClient)
+            {
+                TryDestroy(photon);
+            }
         }
 
         foreach(var type in photonTypes)
