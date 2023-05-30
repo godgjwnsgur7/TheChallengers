b03ce4f41d118ef9ea8a7f07b3cdb3b556f41fb7
godgjwnsgur7 <godgjwnsgur7@naver.com>
2023-05-30 20:56:44 +0900

    [준혁] 결과창 하는중
    .

--- FightingGame/Assets/Scripts/UI/BattleCanvas/ResultWindowUI.cs
+++ FightingGame/Assets/Scripts/UI/BattleCanvas/ResultWindowUI.cs
@@ -3,18 +3,43 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using FGDefine;
+using System;
 
 public class ResultWindowUI : MonoBehaviour
 {
     [SerializeField] RankingScoreUI rankingScore;
-    [SerializeField] Image backgroundImage;
+
+    [SerializeField] Image resultLogoImage;
+
+    [SerializeField] Text matchTypeText;
+    [SerializeField] Text playTimeText;
+
     [SerializeField] Text notifyCountText;
+    [SerializeField] Text timerText;
 
     Coroutine counterCorotine;
 
     int timeCount;
     long myScore;
+    long myWinningRate;
     long enemyScore;
+
+    private void OnEnable()
+    {
+        Test();
+    }
+
+    private void Test()
+    {
+        rankingScore.Open_Score((long)1500, (long)58);
+
+        Invoke("Test2", 1f);
+    }
+
+    private void Test2()
+    {
+        rankingScore.Update_Score((long)1450, (long)36);
+    }
 
     private void OnDisable()
     {
@@ -24,45 +49,54 @@ public class ResultWindowUI : MonoBehaviour
 
     public void Open(bool isDraw, bool isWin = true)
     {
-        myScore = Managers.Network.Get_DBUserData(PhotonLogicHandler.IsMasterClient).ratingPoint;
+        // DB 관련 세팅 (점수, 승률, 랭크 등)
+        DBUserData myDBData = Managers.Network.Get_DBUserData(PhotonLogicHandler.IsMasterClient);
+        myScore = myDBData.ratingPoint;
+        myWinningRate = myDBData.victoryPoint / (myDBData.victoryPoint + myDBData.defeatPoint) * 100;
         enemyScore = Managers.Network.Get_DBUserData(!PhotonLogicHandler.IsMasterClient).ratingPoint;
+        rankingScore.Open_Score(myScore, myWinningRate);
+
+        // 플레이 타임 세팅
+        string[] timer = timerText.text.Split(':');
+        int remainingTimeSecond = int.Parse(timer[0]) * 60 + int.Parse(timer[1]);
+        int playTimeSecond = (int)Managers.Data.gameInfo.maxGameRunTime - remainingTimeSecond;
+        playTimeText.text = String.Format("{0:00} : {1:00}", playTimeSecond / 60, playTimeSecond % 60);
 
         timeCount = 3;
 
+        // 매치 타입에 따른 세팅
         bool isMatching = PhotonLogicHandler.Instance.CurrentLobbyType == ENUM_MATCH_TYPE.RANDOM;
-
+        matchTypeText.text = isMatching ? "RANDOM MATCH" : "1 ON 1 MATCH";
         if (isMatching)
         {
             timeCount = 5;
-            rankingScore.Open_Score(myScore);
-
+            
             // 게임 중에 팅기거나 그랬을 때, 등의 예외상황 처리가 아직 안되어있음
             myScore = RankingScoreOperator.Operator_RankingScore(isDraw, isWin, myScore, enemyScore);
             Managers.Platform.DBUpdate(DB_CATEGORY.RatingPoint, myScore);
             if(!isDraw)
             {
                 if(isWin)
+                {
                     Managers.Platform.DBUpdate(DB_CATEGORY.VictoryPoint, Managers.Network.Get_DBUserData(PhotonLogicHandler.IsMasterClient).victoryPoint + 1);
+                    myWinningRate = (myDBData.victoryPoint + 1) / (myDBData.victoryPoint + myDBData.defeatPoint + 1) * 100;
+                }
                 else
+                {
                     Managers.Platform.DBUpdate(DB_CATEGORY.DefeatPoint, Managers.Network.Get_DBUserData(PhotonLogicHandler.IsMasterClient).defeatPoint + 1);
+                    myWinningRate = myDBData.victoryPoint / (myDBData.victoryPoint + myDBData.defeatPoint + 1) * 100;
+                }       
             }
         }
 
+        // UI 이미지 세팅
         if (isDraw)
-        {
-            backgroundImage.sprite = Managers.Resource.Load<Sprite>($"Art/Sprites/Result/Draw_Popup");
-        }
+            resultLogoImage.sprite = Managers.Resource.Load<Sprite>($"Art/Sprites/Result_Draw");
         else
-        {
             if (isWin)
-            {
-                backgroundImage.sprite = Managers.Resource.Load<Sprite>($"Art/Sprites/Result/Win_Popup");
-            }
+                resultLogoImage.sprite = Managers.Resource.Load<Sprite>($"Art/Sprites/Result_Victory");
             else
-            {
-                backgroundImage.sprite = Managers.Resource.Load<Sprite>($"Art/Sprites/Result/Lose_Popup");
-            }
-        }
+                resultLogoImage.sprite = Managers.Resource.Load<Sprite>($"Art/Sprites/Result_Defeat");
 
         notifyCountText.text = $"{timeCount}초 뒤에 로비로 이동합니다.";
         gameObject.SetActive(true);
@@ -97,6 +131,6 @@ public class ResultWindowUI : MonoBehaviour
     {
         yield return new WaitForSeconds(delayTime);
 
-        rankingScore.Update_Score(myScore);
+        rankingScore.Update_Score(myScore, myWinningRate);
     }
 }
