f9880c3acdde3f0d4698e9f30cc22fc6c0bad7b0
godgjwnsgur7 <godgjwnsgur7@naver.com>
2023-06-02 19:52:32 +0900

    [준혁] UI 세부조정 (with 가은)
    .

--- FightingGame/Assets/Scripts/InputKey/InputBasicKey.cs
+++ FightingGame/Assets/Scripts/InputKey/InputBasicKey.cs
@@ -21,7 +21,7 @@ public class InputBasicKey : InputKey
     public override void Set_Transparency(float _opacity)
     {
         Color changeColor = iconImage.color;
-        changeColor.a = _opacity;
+        changeColor.a = _opacity * 1.3f;
         iconImage.color = changeColor;
 
         base.Set_Transparency(_opacity);
