fe1cefeb1c8ed704df94361498f0a2f8372cc31e
psh50zmfhtm <psh50zmfhtm@naver.com>
2023-06-07 17:04:02 +0900

    디파인 테스트 모드 리팩토링

--- FightingGame/Assets/Scripts/Debug/CharacterDebugger.cs
+++ FightingGame/Assets/Scripts/Debug/CharacterDebugger.cs
@@ -7,63 +7,114 @@ using System.Linq;
 
 using UnityEditor;
 
-public class CharacterDebugger : AssetPostprocessor
+public class DefineController : AssetPostprocessor
 {
-    private const string TestModeMenuName = "Debug/테스트 모드 세팅";
-	private static string[] Symbols = new string[1]
-	{
-		"TEST_MODE"
-	};
+    private static DefineSetter characterDefineSetter = new DefineSetter(CharacterDamageMenuName, "TEST_MODE");
+    private static DefineSetter googleLoginDefineSetter = new GoogleLoginDefineSetter(GoggleLoginMenuName, "GOOGLE_LOGIN_MODE");
 
-	public static bool IsTestMode
-	{
-		get { return HasTestModeSymbol; }
-	}
+    private const string GoggleLoginMenuName = "Debug/구글 로그인 활성화";
+    private const string CharacterDamageMenuName = "Debug/캐릭터 데미지 치트";
 
-	private static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths)
-	{
-		Menu.SetChecked(TestModeMenuName, IsTestMode);
-	}
-
-	[MenuItem(TestModeMenuName)]
-    public static void SetTestMode()
+    private static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths)
     {
-        Menu.SetChecked(TestModeMenuName, ToggleTestMode()); 
-	}
-
-    private static bool ToggleTestMode()
-    {
-		BuildTargetGroup targetGroup = EditorUserBuildSettings.selectedBuildTargetGroup;
-		string definesString = PlayerSettings.GetScriptingDefineSymbolsForGroup(EditorUserBuildSettings.selectedBuildTargetGroup);
-		List<string> allDefines = definesString.Split(';').ToList();
-
-		if (!IsTestMode) // 기존에 심볼을 가지고 있지 않았다면 추가
-        {
-			allDefines.AddRange(Symbols.Except(allDefines));
-		}
-		else
-        {
-			allDefines = allDefines.Except(Symbols).ToList();
-		}
-
-		PlayerSettings.SetScriptingDefineSymbolsForGroup(
-			targetGroup,
-			string.Join(";", allDefines.ToArray()));
-
-		return IsTestMode;
+        googleLoginDefineSetter.SetDefine();
+        characterDefineSetter.SetDefine();
     }
 
-	private static bool HasTestModeSymbol
-	{
-		get
-		{
-			BuildTargetGroup targetGroup = EditorUserBuildSettings.selectedBuildTargetGroup;
-			string definesString = PlayerSettings.GetScriptingDefineSymbolsForGroup(EditorUserBuildSettings.selectedBuildTargetGroup);
-			List<string> allDefines = definesString.Split(';').ToList();
+    [MenuItem(GoggleLoginMenuName)]
+    public static void SetGoggleLoginMode()
+    {
+        googleLoginDefineSetter.SetTestMode();
+    }
 
-			return Symbols.All(allDefines.Contains);
-		}
-	}
+    [MenuItem(CharacterDamageMenuName)]
+    public static void SetCharacterDamageMode()
+    {
+        characterDefineSetter.SetTestMode();
+    }
+}
+
+public class DefineSetter
+{
+    protected string menuName = null;
+    protected string[] symbols = null;
+
+    public DefineSetter(string menuName, params string[] symbols)
+    {
+        this.menuName = menuName;
+        this.symbols = symbols;
+    }
+
+    public void SetDefine()
+    {
+        Menu.SetChecked(menuName, HasTestModeSymbol);
+        Debug.Log($"{menuName} : {HasTestModeSymbol}");
+    }
+
+    public void SetTestMode()
+    {
+        Menu.SetChecked(menuName, ToggleTestMode());
+        Debug.Log($"{menuName} : {HasTestModeSymbol}");
+    }
+
+    protected virtual bool ToggleTestMode()
+    {
+        BuildTargetGroup targetGroup = EditorUserBuildSettings.selectedBuildTargetGroup;
+        string definesString = PlayerSettings.GetScriptingDefineSymbolsForGroup(EditorUserBuildSettings.selectedBuildTargetGroup);
+        List<string> allDefines = definesString.Split(';').ToList();
+
+        if (!HasTestModeSymbol) // 기존에 심볼을 가지고 있지 않았다면 추가
+        {
+            allDefines.AddRange(symbols.Except(allDefines));
+        }
+        else
+        {
+            allDefines = allDefines.Except(symbols).ToList();
+        }
+
+        PlayerSettings.SetScriptingDefineSymbolsForGroup(
+            targetGroup,
+            string.Join(";", allDefines.ToArray()));
+
+        return HasTestModeSymbol;
+    }
+
+    protected bool HasTestModeSymbol
+    {
+        get
+        {
+            BuildTargetGroup targetGroup = EditorUserBuildSettings.selectedBuildTargetGroup;
+            string definesString = PlayerSettings.GetScriptingDefineSymbolsForGroup(EditorUserBuildSettings.selectedBuildTargetGroup);
+            List<string> allDefines = definesString.Split(';').ToList();
+
+            return symbols.All(allDefines.Contains);
+        }
+    }
+}
+
+public class GoogleLoginDefineSetter : DefineSetter
+{
+    public GoogleLoginDefineSetter(string menuName, params string[] symbols) : base(menuName, symbols)
+    {
+
+    }
+
+    // 무조건 True
+    // 추후엔 FGPlatform Library에 넣는 코드로 수정함
+    //protected override bool ToggleTestMode()
+    //{
+    //    BuildTargetGroup targetGroup = EditorUserBuildSettings.selectedBuildTargetGroup;
+    //    string definesString = PlayerSettings.GetScriptingDefineSymbolsForGroup(EditorUserBuildSettings.selectedBuildTargetGroup);
+    //    List<string> allDefines = definesString.Split(';').ToList();
+
+    //    allDefines.AddRange(symbols.Except(allDefines));
+
+    //    PlayerSettings.SetScriptingDefineSymbolsForGroup(
+    //        targetGroup,
+    //        string.Join(";", allDefines.ToArray()));
+
+    //    return HasTestModeSymbol;
+    //}
 }
 
 #endif
