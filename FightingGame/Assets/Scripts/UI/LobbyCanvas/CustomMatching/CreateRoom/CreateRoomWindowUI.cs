a85f14efde058bc2f631764243890114141bf45a
godgjwnsgur7 <godgjwnsgur7@naver.com>
2023-06-17 19:23:44 +0900

    [준혁] 방 생성 UI 리소스 적용 및 기능구현
    .

--- FightingGame/Assets/Scripts/UI/LobbyCanvas/CustomMatching/CreateRoom/CreateRoomWindowUI.cs
+++ FightingGame/Assets/Scripts/UI/LobbyCanvas/CustomMatching/CreateRoom/CreateRoomWindowUI.cs
@@ -8,14 +8,24 @@ using FGDefine;
 public class CreateRoomWindowUI : UIElement
 {
     [SerializeField] InputField userInputField;
-    [SerializeField] Image inputFieldLineImage;
-    [SerializeField] Image inputFieldEditImage;
+
+    [SerializeField] Image inputFieldBorderImage;
+    [SerializeField] Text errorText;
 
     [SerializeField] CustormRoom_MapInfo mapInfo;
 
     ENUM_MAP_TYPE currMap = ENUM_MAP_TYPE.CaveMap;
 
+    Coroutine errorTextShakeEffectCoroutine;
     bool isLock = false;
+
+    protected override void OnDisable()
+    {
+        base.OnDisable();
+
+        if (errorTextShakeEffectCoroutine != null)
+            StopCoroutine(errorTextShakeEffectCoroutine);
+    }
 
     private void Open_CustomRoom()
     {
@@ -29,7 +39,11 @@ public class CreateRoomWindowUI : UIElement
 
         userInputField.text = "";
         userInputField.characterLimit = Managers.Data.nameTextLimit;
-        
+        inputFieldBorderImage.color = Managers.Data.Get_DeselectColor();
+
+        if (errorText.gameObject.activeSelf)
+            errorText.gameObject.SetActive(false);
+
         userInputField.onValueChanged.RemoveAllListeners(); 
         userInputField.onValueChanged.AddListener(
             (word) => userInputField.text = Regex.Replace(word, @"[^0-9a-zA-Zㄱ-ㅎ가-힣\!\?\~\.\,\s)]", "")
@@ -45,6 +59,7 @@ public class CreateRoomWindowUI : UIElement
     public void Close()
     {
         gameObject.SetActive(false);
+        errorText.gameObject.SetActive(false);
     }
     public override void OnClick_Exit()
     {
@@ -61,15 +76,19 @@ public class CreateRoomWindowUI : UIElement
 
     public void OnClick_CreatRoom()
     {
-        if (userInputField.text.Trim() == "")
+        if(userInputField.text.Trim() == "")
         {
-            Managers.UI.popupCanvas.Open_NotifyPopup("방 제목을 입력하지 않았습니다.");       
+            ErrorTextShakeEffect("방이름을 입력해주세요.");
+            return;
+        }
+        if (userInputField.text.Length <= 4)
+        {
+            ErrorTextShakeEffect("방이름은 4글자 이상이여야 합니다.");
             return;
         }
         else if(Managers.Data.BadWord_Discriminator(userInputField.text))
         {
-            userInputField.text = "";
-            Managers.UI.popupCanvas.Open_NotifyPopup("올바르지 않은 방 제목입니다.");
+            ErrorTextShakeEffect("사용할 수 없는 방이름입니다.");
             return;
         }
 
@@ -84,15 +103,59 @@ public class CreateRoomWindowUI : UIElement
         Managers.UI.popupCanvas.Play_FadeOutEffect(Open_CustomRoom);
     }
 
+    private void ErrorTextShakeEffect(string _errorMessage = "")
+    {
+        if (errorTextShakeEffectCoroutine != null)
+            StopCoroutine(errorTextShakeEffectCoroutine);
+
+        ColorUtility.TryParseHtmlString("#E74341", out Color color);
+        inputFieldBorderImage.color = color;
+
+        errorText.text = "* " + _errorMessage;
+
+        if (errorText.gameObject.activeSelf == false)
+            errorText.gameObject.SetActive(true);
+
+        errorTextShakeEffectCoroutine = StartCoroutine(IErrorTextShakeEffect());
+    }
+
     public void InputField_ValueChange()
     {
-        inputFieldLineImage.color = Managers.Data.Get_SelectColor();
-        inputFieldEditImage.color = Managers.Data.Get_SelectColor();
+        if (errorTextShakeEffectCoroutine != null)
+            StopCoroutine(errorTextShakeEffectCoroutine);
+
+        inputFieldBorderImage.color = Managers.Data.Get_SelectColor();
+
+        if (errorText.gameObject.activeSelf)
+            errorText.gameObject.SetActive(false);
     }
 
     public void InputField_EndEdit()
     {
-        inputFieldLineImage.color = Managers.Data.Get_DeselectColor();
-        inputFieldEditImage.color = Managers.Data.Get_DeselectColor();
+        if (errorTextShakeEffectCoroutine != null)
+            StopCoroutine(errorTextShakeEffectCoroutine);
+
+        inputFieldBorderImage.color = Managers.Data.Get_DeselectColor();
+    }
+
+    protected IEnumerator IErrorTextShakeEffect()
+    {
+        float shakeTime = 0.5f;
+        float shakePower = 0.05f;
+        Vector2 originVec = errorText.gameObject.transform.position;
+
+        float realTime = 0f;
+
+        while(realTime < shakeTime)
+        {
+            realTime += Time.deltaTime;
+            
+            errorText.gameObject.transform.position = originVec + (Vector2)Random.insideUnitCircle * shakePower;
+
+            yield return null;
+        }
+
+        errorText.gameObject.transform.position = originVec;
+        errorTextShakeEffectCoroutine = null;
     }
 }
