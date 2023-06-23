92775c4d77fd0fe05416d092fdd51dae3776075f
godgjwnsgur7 <godgjwnsgur7@naver.com>
2023-06-23 20:50:49 +0900

    [준혁] 훈련장 메뉴 넣고 뺴는 기능 추가
    .

--- FightingGame/Assets/Scripts/UI/TrainingCanvas/TrainingMenuWindowUI.cs
+++ FightingGame/Assets/Scripts/UI/TrainingCanvas/TrainingMenuWindowUI.cs
@@ -8,11 +8,18 @@ public class TrainingMenuWindowUI : UIElement
 {
     TrainingScene trainingScene;
 
+    float originalPosX;
+    bool isHideState = false;
+
+    Coroutine moveToWindowCoroutine;
+
     public void Init()
     {
         trainingScene = Managers.Scene.CurrentScene.GetComponent<TrainingScene>();
+        originalPosX = this.transform.localPosition.x;
+        isHideState = false;
     }
-    
+
     public void OnClick_ChangeMap()
     {
         Managers.UI.popupCanvas.Open_MapSelectPopup(trainingScene.Change_CurrMap);
@@ -44,4 +51,40 @@ public class TrainingMenuWindowUI : UIElement
         (GoTo_LobbyScene, null, "로비로 돌아가시겠습니까?");
 
     private void GoTo_LobbyScene() => Managers.Scene.LoadScene(ENUM_SCENE_TYPE.Lobby);
+
+    public void OnClick_PushOut()
+    {
+        if (moveToWindowCoroutine != null)
+            return;
+
+        if(!isHideState)
+        {
+            isHideState = true;
+            moveToWindowCoroutine = StartCoroutine(IMoveToWindow(originalPosX - 500f));
+        }
+        else
+        {
+            isHideState = false;
+            moveToWindowCoroutine = StartCoroutine(IMoveToWindow(originalPosX));
+        }
+    }
+
+    protected IEnumerator IMoveToWindow(float movePosX)
+    {
+        float time = 0;
+        float currPosX = this.transform.localPosition.x;
+
+        while(movePosX != this.transform.localPosition.x)
+        {
+            time += Time.deltaTime * 5f;
+
+            this.transform.localPosition = new Vector2(
+                Mathf.Lerp(currPosX, movePosX, time)
+                , this.transform.localPosition.y);
+
+            yield return null;
+        }
+
+        moveToWindowCoroutine = null;
+    }
 }
