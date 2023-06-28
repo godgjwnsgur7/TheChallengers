f9014129fa2ad9c1dc47a769f5897d99d6aae9e6
ParkSolHwi <psh50zmfhtm@gmail.com>
2023-06-28 21:44:13 +0900

    ㅇㄴㄹㄴ
    ㅊㅍㅌㅌㅊㅍ

--- FightingGame/Assets/FGPlatform/Crashlytics/IPlatformCrashlytics.cs
+++ FightingGame/Assets/FGPlatform/Crashlytics/IPlatformCrashlytics.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Firebase.Crashlytics;
+using System;
+
 public interface IPlatformCrashlytics
 {
 	void Init();
@@ -13,7 +15,19 @@ public class PlatformCrashlytics : IPlatformCrashlytics
 	{
 #if UNITY_ANDROID
 		Crashlytics.ReportUncaughtExceptionsAsFatal = true;
+		Crashlytics.IsCrashlyticsCollectionEnabled = true;
 		Firebase.FirebaseApp.LogLevel = Firebase.LogLevel.Debug;
 #endif
 	}
+
+	public void Report(System.Exception e)
+	{
+		Crashlytics.LogException(e);
+	}
+
+	public void Report(string message)
+	{
+		Crashlytics.Log(message);
+	}
+	
 }
